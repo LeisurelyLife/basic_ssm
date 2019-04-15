0e90b41e547e91a490c9fe1a9b494936b38472b1
ruant <1284957262@qq.com>
2019-04-15 16:24:49 +0800

    扩张实体类

---
+++ src/main/java/cn/rt/entity/UserExtend.java
@@ -0,0 +1,17 @@
+package cn.rt.entity;
+
+/**
+ * @author ruanting
+ * @date 2019/04/15
+ */
+public class UserExtend extends User {
+    private String adname;
+
+    public String getAdname() {
+        return adname;
+    }
+
+    public void setAdname(String adname) {
+        this.adname = adname;
+    }
+}
